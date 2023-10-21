e30d46729f13d7566c904ec76461b77a6d3aebca
klayhopgood <klaychop1@gmail.com>
2023-10-21 16:46:44 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+<div class="DesktopHeroBlock3" style="width: 100%; height: 100%; position: relative; background: linear-gradient(0deg, rgba(4, 19, 45, 0.90) 0%, rgba(4, 19, 45, 0.90) 100%)">
+    <div class="DesktopHeader" style="padding-top: 40px; padding-bottom: 32px; left: 48px; top: 0px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: center; gap: 40px; display: inline-flex">
+        <div class="Navigation" style="width: 1344px; justify-content: space-between; align-items: flex-start; display: inline-flex">
+            <div class="PlaceholderLogo" style="width: 48px; height: 124.14px; position: relative; transform: rotate(-90deg); transform-origin: 0 0">
+                <img class="Subtract" style="width: 48px; height: 124.14px; left: 0px; top: 0px; position: absolute; border: 15.40px white solid" src="https://via.placeholder.com/48x124" />
+            </div>
+        </div>
+    </div>
+    <div class="Subheading" style="left: 720px; top: 375px; position: absolute; text-align: center"></div>
+    <div class="Frame992016" style="left: 418px; top: 304px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: center; gap: 32px; display: inline-flex">
+        <div class="Name" style="width: 604px; text-align: center; color: white; font-size: 40px; font-family: Roc Grotesk; font-weight: 500; line-height: 28px; word-wrap: break-word">Launching soon.<br/>Sign up for exclusive access.</div>
+        <div class="NewsletterSignup" style="border-radius: 4px; overflow: hidden; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+            <div class="NewstellerInput" style="width: 380px; height: 48px; padding: 16px; background: white; border-right: 1px #04132D solid; justify-content: flex-start; align-items: center; gap: 8px; display: flex">
+                <div class="Placeholder" style="color: #1E2737; font-size: 14px; font-family: Roc Grotesk; font-weight: 400; line-height: 20px; word-wrap: break-word">Enter your email address</div>
+            </div>
+            <div class="Button" style="width: 52px; padding: 16px; background: #05FF00; justify-content: center; align-items: center; gap: 8px; display: flex">
+                <div class="ChevronRight" style="width: 20px; height: 20px; position: relative">
+                    <div class="Icon" style="width: 5px; height: 10px; left: 7.50px; top: 5px; position: absolute; border: 1.67px #1E2737 solid"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
